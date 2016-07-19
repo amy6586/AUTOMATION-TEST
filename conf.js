f5d2300bee89474e411fc35e7e3b201276c3848d
amy6586 <amy6586@gmail.com>
2016-07-19 11:44:08 -0600

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
    //Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
