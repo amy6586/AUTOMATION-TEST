22089727948e81ac8d934fdda21fe34d004edb7f
amy6586 <amy6586@gmail.com>
2016-07-19 10:10:38 -0600

    Add files via upload
    This Automated test was creates in Protractor, in order to run it you must install Selenium WebDriver, Chrome Driver, node.js and install global Protractor.

---
+++ OUTFITTERY.js
@@ -0,0 +1,41 @@
+describe('To Test OUTFITTERY Login window', function() {
+it('should be able to login', function() {
+browser.driver
+browser.driver.get('https://www.outfittery.com/login/auth');
+browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
+browser.driver.findElement(by.name('j_password')).sendKeys("testing123testing");
+browser.driver.findElement(by.id('submit')).click();
+});
+});
+
+
+describe('To Test OUTFITTERY Login window error WRONG PASSWORD', function() {
+it('should be able to login', function() {
+browser.driver
+browser.driver.get('https://www.outfittery.com/login/auth');
+browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
+browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
+browser.driver.findElement(by.id('submit')).click();
+//Printing Text
+var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
+txt.then(function(text) {
+console.log(text);
+});
+});
+});
+
+
+describe('To Test OUTFITTERY Login window error WRONG uSER NAME', function() {
+it('should be able to login', function() {
+browser.driver
+browser.driver.get('https://www.outfittery.com/login/auth');
+browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@TEST.com");
+browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
+browser.driver.findElement(by.id('submit')).click();
+//Printing Text
+var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
+txt.then(function(text) {
+console.log(text);
+});
+});
+});
