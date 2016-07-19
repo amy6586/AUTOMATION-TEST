eb3cdfcfa3a627a11445a9e9b6b44b9ce6d7be3a
amy6586 <amy6586@gmail.com>
2016-07-19 11:42:46 -0600

    Update OUTFITTERY.js

--- OUTFITTERY.js
+++ OUTFITTERY.js
@@ -1,43 +1,43 @@
 describe ('To Test OUTFITTERY Login Link Home Page', function() {
-	it ('to test login link goes to to the correct page' function(){
-		browser.get('https://www.outfittery.com');
-		element(by.show('user.signedIn')).click();
+        it ('to test login link goes to to the correct page' function(){
+	browser.get('https://www.outfittery.com');
+        element(by.show('user.signedIn')).click();
 	});	
 });
 describe('o Test OUTFITTERY Login Page Postive', function() {
-it('should be able to login', function() {
-browser.driver
-browser.driver.get('https://www.outfittery.com/login/auth');
-browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
-browser.driver.findElement(by.name('j_password')).sendKeys("testing123testing");
-browser.driver.findElement(by.id('submit')).click();
+        it('should be able to login', function() {
+        browser.driver
+        browser.driver.get('https://www.outfittery.com/login/auth');
+        browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
+        browser.driver.findElement(by.name('j_password')).sendKeys("testing123testing");
+        browser.driver.findElement(by.id('submit')).click();
 });
 });
 describe('To Test OUTFITTERY Login window error WRONG PASSWORD', function() {
-it('should be unable to login', function() {
-browser.driver
-browser.driver.get('https://www.outfittery.com/login/auth');
-browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
-browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
-browser.driver.findElement(by.id('submit')).click();
+        it('should be unable to login', function() {
+       browser.driver
+       browser.driver.get('https://www.outfittery.com/login/auth');
+       browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@gmail.com");
+       browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
+       browser.driver.findElement(by.id('submit')).click();
 //Printing Text
-var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
-txt.then(function(text) {
-console.log(text);
+      var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
+      txt.then(function(text) {
+      console.log(text);
 });
 });
 });
 describe('To Test OUTFITTERY Login window error WRONG USER NAME', function() {
-it('should be unable to login', function() {
-browser.driver
-browser.driver.get('https://www.outfittery.com/login/auth');
-browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@TEST.com");
-browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
-browser.driver.findElement(by.id('submit')).click();
+        it('should be unable to login', function() {
+        browser.driver
+        browser.driver.get('https://www.outfittery.com/login/auth');
+        browser.driver.findElement(by.name('j_username')).sendKeys("amytesteng@TEST.com");
+        browser.driver.findElement(by.name('j_password')).sendKeys("testing123tASDASDASDesting");
+        browser.driver.findElement(by.id('submit')).click();
 //Printing Text
-var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
-txt.then(function(text) {
-console.log(text);
+       var txt = browser.driver.findElement(by.PartialLinkText('The combination of e-mail and password is not correct.')).getText()
+       txt.then(function(text) {
+       console.log(text);
 });
 });
 });
